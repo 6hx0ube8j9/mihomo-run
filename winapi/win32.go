d24797c198f04de8322d2ddd298378debcaadf13
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-05-28 17:20:22 +0800

    Update win32.go

--- winapi/win32.go
+++ winapi/win32.go
@@ -11,40 +11,43 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+type ConfigContextInterface interface {
+	CompareAndSwapFocusing(oldVal, newVal int32) bool
+	SetFocusing(val int32)
+}
+
 var (
 	u32       = windows.NewLazySystemDLL("user32.dll")
 	k32       = windows.NewLazySystemDLL("kernel32.dll")
 	wininet   = windows.NewLazySystemDLL("wininet.dll")
-	imghelp   = windows.NewLazySystemDLL("imagehlp.dll")
 	setOption = wininet.NewProc("InternetSetOptionW")
 )
 
 var (
-	procEnumWindows      = u32.NewProc("EnumWindows")
-	procGetClassName     = u32.NewProc("GetClassNameW")
-	procIsWindowVisible  = u32.NewProc("IsWindowVisible")
-	procGetWindowThread  = u32.NewProc("GetWindowThreadProcessId")
-	procGetWindow        = u32.NewProc("GetWindow")
-	procGetWindowText    = u32.NewProc("GetWindowTextW")
-	procSetWindowPos     = u32.NewProc("SetWindowPos")
-	procShowWindow       = u32.NewProc("ShowWindow")
-	procSetForeground    = u32.NewProc("SetForegroundWindow")
-	procBringToTop       = u32.NewProc("BringWindowToTop")
-	procGetForeground    = u32.NewProc("GetForegroundWindow")
-	procAttachThread     = u32.NewProc("AttachThreadInput")
-	procGetCurrentThread = k32.NewProc("GetCurrentThreadId")
-	procKeybdEvent       = u32.NewProc("keybd_event")
-	procGetSystemMetrics = u32.NewProc("GetSystemMetrics")
+	procEnumWindows        = u32.NewProc("EnumWindows")
+	procGetClassName       = u32.NewProc("GetClassNameW")
+	procIsWindowVisible    = u32.NewProc("IsWindowVisible")
+	procGetWindowThread    = u32.NewProc("GetWindowThreadProcessId")
+	procGetWindow          = u32.NewProc("GetWindow")
+	procGetWindowText      = u32.NewProc("GetWindowTextW")
+	procSetWindowPos       = u32.NewProc("SetWindowPos")
+	procShowWindow         = u32.NewProc("ShowWindow")
+	procSetForeground      = u32.NewProc("SetForegroundWindow")
+	procBringToTop         = u32.NewProc("BringWindowToTop")
+	procGetForeground      = u32.NewProc("GetForegroundWindow")
+	procAttachThread       = u32.NewProc("AttachThreadInput")
+	procGetCurrentThread   = k32.NewProc("GetCurrentThreadId")
+	procKeybdEvent         = u32.NewProc("keybd_event")
+	procGetSystemMetrics   = u32.NewProc("GetSystemMetrics")
+	procSwitchToThisWindow = u32.NewProc("SwitchToThisWindow")
 )
 
 const (
-	SW_RESTORE     = 9
-	SWP_NOSIZE     = 0x0001
-	SWP_NOMOVE     = 0x0002
+	SW_RESTORE   = 9
+	SWP_NOSIZE   = 0x0001
+	SWP_NOMOVE   = 0x0002
 	SWP_SHOWWINDOW = 0x0040
-	SWP_SILKY      = SWP_NOSIZE | SWP_NOMOVE | SWP_SHOWWINDOW
-	SM_CXSCREEN    = 0
-	SM_CYSCREEN    = 1
+	SWP_SILKY    = SWP_NOSIZE | SWP_NOMOVE | SWP_SHOWWINDOW
 )
 
 var cachedWebUIHwnd atomic.Uintptr
@@ -52,11 +55,10 @@ var cachedWebUIHwnd atomic.Uintptr
 func init() {
 	procSetContext := u32.NewProc("SetProcessDpiAwarenessContext")
 	_, _, err := procSetContext.Call(uintptr(0xfffffffc))
-	if err == nil || uint32(err.(syscall.Errno)) == 0 {
-		return
+	if err != nil && uint32(err.(syscall.Errno)) != 0 {
+		procSetAware := u32.NewProc("SetProcessDPIAware")
+		_, _, _ = procSetAware.Call()
 	}
-	procSetAware := u32.NewProc("SetProcessDPIAware")
-	_, _, _ = procSetAware.Call()
 }
 
 func GetCachedWebUIHwnd() uintptr {
@@ -82,12 +84,7 @@ func RefreshInternetOptions() {
 	_, _, _ = setOption.Call(0, 39, 0, 0)
 }
 
-type configContextInterface interface {
-	CompareAndSwapFocusing(oldVal, newVal int32) bool
-	SetFocusing(val int32)
-}
-
-func FocusWindowSilky(targetHwnd uintptr, cm configContextInterface) {
+func FocusWindowSilky(targetHwnd uintptr, cm ConfigContextInterface) {
 	runtime.LockOSThread()
 	defer runtime.UnlockOSThread()
 
@@ -111,14 +108,9 @@ func FocusWindowSilky(targetHwnd uintptr, cm configContextInterface) {
 	}
 
 	procShowWindow.Call(targetHwnd, SW_RESTORE)
-
-	winuser := windows.NewLazySystemDLL("user32.dll")
-	switchToThisWindow := winuser.NewProc("SwitchToThisWindow")
-	_, _, _ = switchToThisWindow.Call(targetHwnd, 1)
-
+	procSwitchToThisWindow.Call(targetHwnd, 1)
 	procSetForeground.Call(targetHwnd)
 	procBringToTop.Call(targetHwnd)
-
 	procSetWindowPos.Call(targetHwnd, uintptr(0xFFFFFFFFFFFFFFFF), 0, 0, 0, 0, SWP_SILKY)
 
 	if targT != 0 && targT != currT {
@@ -135,7 +127,7 @@ func FocusWindowSilky(targetHwnd uintptr, cm configContextInterface) {
 	})
 }
 
-func FindAndFocusChromeWindow(mainPid uint32, cm configContextInterface) bool {
+func FindAndFocusChromeWindow(mainPid uint32, cm ConfigContextInterface) bool {
 	runtime.LockOSThread()
 	defer runtime.UnlockOSThread()
 
@@ -156,27 +148,13 @@ func FindAndFocusChromeWindow(mainPid uint32, cm configContextInterface) bool {
 					SetCachedWebUIHwnd(hwnd)
 					return 0
 				}
-
-				childCount := 0
-				child, _, _ := procGetWindow.Call(hwnd, 5)
-				for child != 0 {
-					childCount++
-					if childCount > 5 {
-						break
-					}
-					child, _, _ = procGetWindow.Call(child, 2)
-				}
-
-				if childCount <= 5 {
-					var titleBuf [512]uint16
-					_, _, _ = procGetWindowText.Call(hwnd, uintptr(unsafe.Pointer(&titleBuf[0])), 512)
-					wndTitle := strings.ToLower(windows.UTF16ToString(titleBuf[:]))
-
-					if strings.Contains(wndTitle, "ui") || strings.Contains(wndTitle, "dashboard") || strings.Contains(wndTitle, "proxies") {
-						foundHwnd = hwnd
-						SetCachedWebUIHwnd(hwnd)
-						return 0
-					}
+				var titleBuf [512]uint16
+				_, _, _ = procGetWindowText.Call(hwnd, uintptr(unsafe.Pointer(&titleBuf[0])), 512)
+				wndTitle := strings.ToLower(windows.UTF16ToString(titleBuf[:]))
+				if strings.Contains(wndTitle, "ui") || strings.Contains(wndTitle, "dashboard") {
+					foundHwnd = hwnd
+					SetCachedWebUIHwnd(hwnd)
+					return 0
 				}
 			}
 		}
