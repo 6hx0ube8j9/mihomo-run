13a26242a06517a69ba1b0020c73527db5779385
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-05-31 13:25:49 +0800

    Update registry.go

--- sysproxy/registry.go
+++ sysproxy/registry.go
@@ -55,10 +55,13 @@ func (pm *ProxyManager) SetProxyRegistry(enable bool) {
 			port = "7890"
 		}
 		server := "127.0.0.1:" + port
-		bypass := "<local>;localhost;127.*;10.*;172.16.*;192.168.*"
-		setNativeProxy(server, bypass)
+		bypass := "<local>;localhost;127.*;10.*;192.168.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;169.254.*;::1"
+		
+		setNativeProxy(server, bypass)		
+		pm.cm.SaveJsonConfig("proxy", "true") 
 	} else {
 		clearNativeProxy()
+		pm.cm.SaveJsonConfig("proxy", "false")
 	}
 }
 
