9425c47b99b37df3f9a4017e6d1647f4b5aaa554
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-06-12 11:32:10 +0800

    Update registry.go

--- sysproxy/registry.go
+++ sysproxy/registry.go
@@ -1,207 +1,112 @@
 package sysproxy
 
 import (
-	"errors"
 	"strconv"
-	"syscall"
 	"time"
 
 	"golang.org/x/sys/windows"
 	"golang.org/x/sys/windows/registry"
-)
 
-const (
-	REG_PROXY                        = `Software\Microsoft\Windows\CurrentVersion\Internet Settings`
-	INTERNET_OPTION_SETTINGS_CHANGED = 39
-	INTERNET_OPTION_REFRESH          = 37
+	"mihomo-run/config"
 )
 
 var (
-	modWininet         = windows.NewLazySystemDLL("wininet.dll")
-	procInternetSetOpt = modWininet.NewProc("InternetSetOptionW")
+	wininet   = windows.NewLazySystemDLL("wininet.dll")
+	setOption = wininet.NewProc("InternetSetOptionW")
 )
 
-type ConfigInterface interface {
-	GetJsonConfig(key string) string
-	SaveJsonConfig(key, value string)
-	GetLastAppliedProxy() bool
-	SetLastAppliedProxy(enable bool)
-	GetProxyState() bool
-	SetProxyState(enable bool)
-	IsReallyExiting() bool
-	IsProxyWriting() bool
-	SetProxyWriting(val bool)
-}
+type Win32NotificationBridge struct{}
 
-type Win32APIInterface interface {
-	RefreshInternetOptions()
+func (b *Win32NotificationBridge) Refresh() {
+	_, _, _ = setOption.Call(0, 37, 0, 0)
+	_, _, _ = setOption.Call(0, 39, 0, 0)
 }
 
 type ProxyManager struct {
-	cm  ConfigInterface
-	win Win32APIInterface
+	cm *config.ConfigManager
+	nb *Win32NotificationBridge
 }
 
-func NewProxyManager(cm ConfigInterface, win Win32APIInterface) *ProxyManager {
+func NewProxyManager(cm *config.ConfigManager, nb *Win32NotificationBridge) *ProxyManager {
 	return &ProxyManager{
-		cm:  cm,
-		win: win,
+		cm: cm,
+		nb: nb,
 	}
 }
 
 func (pm *ProxyManager) SetProxyRegistry(enable bool) {
-	key, err := registry.OpenKey(registry.CURRENT_USER, REG_PROXY, registry.QUERY_VALUE|registry.SET_VALUE)
+	if pm.cm.IsProxyWriting() {
+		return
+	}
+	pm.cm.SetProxyWriting(true)
+	defer pm.cm.SetProxyWriting(false)
+
+	if pm.cm.GetLastAppliedProxy() == enable {
+		return
+	}
+
+	k, err := registry.OpenKey(registry.CURRENT_USER, `Software\Microsoft\Windows\CurrentVersion\Internet Settings`, registry.SET_VALUE)
 	if err != nil {
 		return
 	}
-	defer key.Close()
-	
-    pm.cm.SetProxyWriting(true)
-	defer pm.cm.SetProxyWriting(false)
-	
-	currentEnable, _, err := key.GetIntegerValue("ProxyEnable")
-	realEnabled := (err == nil && currentEnable == 1)
-
-	if pm.cm.GetLastAppliedProxy() == enable && realEnabled == enable {
-		return
-	}
-
-	success := false
+	defer k.Close()
 
 	if enable {
 		port := pm.cm.GetJsonConfig("port")
-		if port == "" || len(port) < 4 {
+		if port == "" {
 			port = "7890"
 		}
-		serverStr := "127.0.0.1:" + port
-
-		errServer := key.SetStringValue("ProxyServer", serverStr)
-		errEnable := key.SetDWordValue("ProxyEnable", 1)
-		bypassStr := "<local>;localhost;127.*;10.*;100.64.*;169.254.*;172.16.*;192.168.*"
-		errBypass := key.SetStringValue("ProxyOverride", bypassStr)
-
-		errPac := key.DeleteValue("AutoConfigURL")
-		if errPac != nil && !errors.Is(errPac, syscall.ERROR_FILE_NOT_FOUND) {
-		}
-
-		if errServer == nil && errEnable == nil && errBypass == nil {
-			success = true
-		} else {
-			_ = key.SetDWordValue("ProxyEnable", 0)
-		}
+		_ = k.SetDWordValue("ProxyEnable", 1)
+		_ = k.SetStringValue("ProxyServer", "127.0.0.1:"+port)
+		_ = k.SetStringValue("ProxyOverride", "<local>;localhost;127.*;10.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;192.168.*")
 	} else {
-		errEnable := key.SetDWordValue("ProxyEnable", 0)
-
-		if errEnable == nil {
-			success = true
-		}
+		_ = k.SetDWordValue("ProxyEnable", 0)
 	}
 
-	if success {
-		pm.cm.SetLastAppliedProxy(enable)
-		pm.cm.SetProxyState(enable)
-
-		if !pm.cm.IsReallyExiting() {
-			pm.cm.SaveJsonConfig("proxy", strconv.FormatBool(enable))
-		}
-
-		if pm.win != nil {
-			if pm.cm.IsReallyExiting() {
-				pm.win.RefreshInternetOptions()
-			} else {
-				go func() {
-					pm.win.RefreshInternetOptions()
-				}()
-			}
-		}
-	}
+	pm.nb.Refresh()
+	pm.cm.SetLastAppliedProxy(enable)
 }
 
 func (pm *ProxyManager) WatchProxyRegistry() {
-	key, err := registry.OpenKey(registry.CURRENT_USER, REG_PROXY, registry.NOTIFY|registry.QUERY_VALUE)
+	k, err := registry.OpenKey(registry.CURRENT_USER, `Software\Microsoft\Windows\CurrentVersion\Internet Settings`, registry.NOTIFY|registry.QUERY_VALUE)
 	if err != nil {
 		return
 	}
-	defer key.Close()
-
-	var retryCount int
+	defer k.Close()
 
 	for {
 		if pm.cm.IsReallyExiting() {
 			return
 		}
 
-		err := windows.RegNotifyChangeKeyValue(
-			windows.Handle(key),
-			false,
-			windows.REG_NOTIFY_CHANGE_LAST_SET,
-			0,
-			false,
-		)
-
+		event, _ := windows.CreateEvent(nil, 0, 0, nil)
+		err = windows.RegNotifyChangeKeyValue(windows.Handle(k), false, windows.REG_NOTIFY_CHANGE_LAST_SET, event, true)
 		if err != nil {
-			time.Sleep(2 * time.Second)
+			time.Sleep(1 * time.Second)
 			continue
 		}
+
+		s, _ := windows.WaitForSingleObject(event, 2000)
+		windows.CloseHandle(event)
 
 		if pm.cm.IsReallyExiting() {
 			return
 		}
 
-		if pm.cm.IsProxyWriting() {
-			time.Sleep(100 * time.Millisecond)
-			continue
-		}	
-			
-		expectedProxy := pm.cm.GetProxyState()
-
-		if !expectedProxy {
-			retryCount = 0
-			continue
-		}
-		
-		val, _, err := key.GetIntegerValue("ProxyEnable")
-		realProxy := (err == nil && val == 1)
-
-		serverStr, _, errStr := key.GetStringValue("ProxyServer")
-		expectedPort := pm.cm.GetJsonConfig("port")
-		if expectedPort == "" || len(expectedPort) < 4 {
-			expectedPort = "7890"
-		}
-		expectedServer := "127.0.0.1:" + expectedPort
-		
-		isPortHijacked := (errStr == nil && serverStr != expectedServer)
-
-		if realProxy && isPortHijacked {
-			retryCount = 0
-			pm.cm.SetLastAppliedProxy(false)
-			pm.cm.SetProxyState(false)
-			pm.cm.SaveJsonConfig("proxy", "false")
-			continue
-		}
-
-		if !realProxy {
-			retryCount++
-			if retryCount > 3 {
-				pm.cm.SetLastAppliedProxy(false)
-				pm.cm.SetProxyState(false)
-				pm.cm.SaveJsonConfig("proxy", "false")
-				retryCount = 0
+		if s == windows.WAIT_OBJECT_0 {
+			if pm.cm.IsProxyWriting() || pm.cm.IsSystemInitializing() {
 				continue
 			}
 
-			time.Sleep(200 * time.Millisecond)
-			pm.cm.SetLastAppliedProxy(false)
-			pm.SetProxyRegistry(true)
-		} else {
-			retryCount = 0
+			val, _, err := k.GetIntegerValue("ProxyEnable")
+			if err == nil {
+				isRegEnabled := val == 1
+				if isRegEnabled != pm.cm.GetProxyState() {
+					pm.cm.SetProxyState(isRegEnabled)
+					pm.cm.SaveJsonConfig("proxy", strconv.FormatBool(isRegEnabled))
+					pm.cm.SetLastAppliedProxy(isRegEnabled)
+				}
+			}
 		}
 	}
-}
-
-type Win32NotificationBridge struct{}
-
-func (w *Win32NotificationBridge) RefreshInternetOptions() {
-	_, _, _ = procInternetSetOpt.Call(0, INTERNET_OPTION_SETTINGS_CHANGED, 0, 0)
-	_, _, _ = procInternetSetOpt.Call(0, INTERNET_OPTION_REFRESH, 0, 0)
 }
